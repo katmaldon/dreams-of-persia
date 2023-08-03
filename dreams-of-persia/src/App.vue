6c3ce0a82bde6f1bc96bebad9696546520ad405b
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2023-08-03 10:42:30 -0400

    card layouts

--- dreams-of-persia/src/App.vue
+++ dreams-of-persia/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!-- <Nav /> -->
     <section class="parallax-container">
       <Header />
     </section>
@@ -9,16 +8,15 @@
       <Author />
       <Book />
       <Buy />
+      <section class="buffer"></section>
+      <Contact />
     </section>
-    <section class="buffer"></section>
-    <Contact />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import Nav from "./components/Nav.vue";
 import Author from "./components/Author.vue";
 import Book from "./components/Book.vue";
 import Buy from "./components/Buy.vue";
@@ -29,7 +27,6 @@ export default {
   name: "app",
   components: {
     Header,
-    Nav,
     Author,
     Book,
     Buy,
