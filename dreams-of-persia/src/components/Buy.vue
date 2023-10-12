be439e031d513e4fb6e024d7c52e86335585f6f3
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2023-10-12 11:04:43 -0400

    style tweaks

--- dreams-of-persia/src/components/Buy.vue
+++ dreams-of-persia/src/components/Buy.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="card buy">
+    <div class="card__text">
+      <div class="card__title">{{ title }}</div>
+      <p>You can find our book at many great retailers:</p>
+    </div>
     <div class="buy__row">
-      <div class="card__text">
-        <div class="card__title">{{ title }}</div>
-        <p>You can find Dreams of Persia at many great retailers:</p>
+      <div class="buy__column--amazon">
+        <a
+          href="https://store.bookbaby.com/book/dreams-of-persia"
+          target="blank"
+          ><img src="/src/assets/img/BookBabyLogo.svg"
+        /></a>
       </div>
-      <div class="buy__column">
-        <div class="buy__column--amazon">Amazon</div>
-        <div class="buy__column--bookbaby">bookbaby</div>
-        <div class="buy__column--bn">barnes and noble</div>
+      <div class="buy__column--bookbaby">
+        <a
+          href="https://store.bookbaby.com/book/dreams-of-persia"
+          target="blank"
+          ><img src="/src/assets/img/BookBabyLogo.svg"
+        /></a>
       </div>
     </div>
   </div>
@@ -24,3 +33,39 @@ export default {
   },
 };
 </script>
+
+.buy {
+    min-height: 3rem;
+    background-image: linear-gradient(#881c2d, #a72b3e);
+
+    &__row {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 0rem 5rem 5rem 5rem;
+        column-gap: 5rem;
+    }
+
+    &__column {
+        display: flex;
+        flex-direction: column;
+        row-gap: 3rem;
+        min-width: 50%;
+        padding-top: 3rem;
+        align-items: center;
+
+        &--amazon {
+            width: 100px;
+            height: 50px;
+            outline: #fff;
+            background-image: "/scr/assets/img/BookBabyLogo.svg";
+        }
+
+        &--bookbaby {
+            width: 100px;
+            height: 50px;
+            background-image: "/scr/assets/img/BookBabyLogo.svg";
+        }
+    }
+}
--- dreams-of-persia/src/components/Contact.vue
+++ dreams-of-persia/src/components/Contact.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="card contact">
     <div class="card__text">
-      <div class="card__title">{{ title }}</div>
-      <p>For all media inquiries, please use the form below to get in touch.</p>
+      <p>
+        For all media inquiries, please
+        <a href="mailto:someone@example.com">email us</a> to get in touch.
+      </p>
     </div>
-    <div class="contact__form">form</div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: "CONTACT",
-    };
-  },
-};
+export default {};
 </script>
+
+
