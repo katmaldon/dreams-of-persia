fcf7444a0c1a216dc0e509ebf914eb0eaf29a407
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2024-03-03 22:54:07 -0500

    bcopyright update

--- dreams-of-persia/src/components/Footer.vue
+++ dreams-of-persia/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <div>Copyright 2023 Ab Sadeghi-Nejad, MD. All rights reserved</div>
+    <div>Copyright 2024 Ab Sadeghi-Nejad, MD. All rights reserved</div>
   </footer>
 </template>
 
